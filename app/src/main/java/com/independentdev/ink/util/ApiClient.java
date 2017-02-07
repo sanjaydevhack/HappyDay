3e9bd36822a9d11db7b478feb00b9b4db00b0723
sanjayjith.madhavan <sanjayjith.madhavan@object-frontier.com>
2017-02-07 20:42:10 +0530

    Added Firebase push notification and its services

--- app/src/main/java/com/independentdev/ink/util/ApiClient.java
+++ app/src/main/java/com/independentdev/ink/util/ApiClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    private static final String BASE_URL = "https://together-4b63f.firebaseio.com/";
+    private static final String BASE_URL = "https://ink-mobile.firebaseio.com/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
